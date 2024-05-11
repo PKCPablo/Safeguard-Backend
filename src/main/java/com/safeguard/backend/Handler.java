610c6a44e1c897641307fac1d6ddbf0f15cf9238
Pablo <pablinelchulin@gmail.com>
2024-05-11 20:40:48 +0200

    backend remake

--- src/main/java/com/safeguard/backend/Handler.java
+++
@@ -1,6 +0,0 @@
-package com.safeguard.backend;
-
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-public class Handler implements RequestHandler<> {
-}
--- src/main/java/com/safeguard/backend/Request.java
+++
@@ -1,4 +0,0 @@
-package com.safeguard.backend;
-
-public class Request {
-}
